e71e1a50cc71e1892c98ec179b29854ebd338466
Elena Cherny <elta20042004@gmail.com>
2015-08-15 21:26:58 +0300

    Even Tree

--- HackerRank/Even Tree/Program.cs
+++ HackerRank/Even Tree/Program.cs
@@ -9,47 +9,54 @@ namespace Even_Tree
     class Program
     {
         public static int[,] Matrix;
-        public static int[] Konveer;
-        public static int n;
         private static int start;
-        private static int counter=1;
-
+        private static int n;
+        private static int counter = 1;
+        private static int temp = 0;
+        private static int k = 0;
 
         public static void Recursiya(int start)
         {
+            if (k == n)
+            {
+                return;
+            }
 
-          
-        
+            for (int i = 1; i <= n; i++)
+            {
+                k = i;
+                if (Matrix[start, i] == 1)
+                {
+                    temp++;
+                    start = i;
+                    Matrix[start, i] = -1;   //ya tut byla
+                    Recursiya(start);
+                }
+            }
+
+
 
         }
 
 
         private static void Main(string[] args)
         {
-            int t = int.Parse(Console.ReadLine());
-            for (int i = 0; i < t; i++)
+            string[] length = Console.ReadLine().Split(' ');
+            n = int.Parse(length[0]);
+            int m = int.Parse(length[1]);
+
+            Matrix = new int[n + 1, n + 1];
+            for (int j = 0; j < m; j++)
             {
-                string[] length = Console.ReadLine().Split(' ');
-                n = int.Parse(length[0]);
-                int m = int.Parse(length[1]);
+                string[] shura = Console.ReadLine().Split(' ');
+                int x = int.Parse(shura[0]);
+                int y = int.Parse(shura[1]);
 
-                Matrix = new int[n + 1, n + 1];
-                for (int j = 0; j < m; j++)
-                {
-                    string[] shura = Console.ReadLine().Split(' ');
-                    int x = int.Parse(shura[0]);
-                    int y = int.Parse(shura[1]);
-
-                    Matrix[x, y] = 1;
-                    Matrix[y, x] = 1;
-                }
-
-                string startString = Console.ReadLine();
-                start = int.Parse(startString);
-                Konveer = new int[n + 1];
-                Konveer[start] = 1;
-                Recursiya(start);
+                Matrix[y, x] = 1;
             }
+            start = 1;
+            Recursiya(start);
+
         }
     }
 }
