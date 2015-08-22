259b6806764adb47871d0a7f217840ed4e305eab
Elena Cherny <elta20042004@gmail.com>
2015-08-23 00:46:23 +0300

    80 pointov ;))

---
+++ HackerRank/Matrix Rotation 2/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_Rotation
+{
+    class Program
+    {
+        static int[,] matrix;
+        static int[,] matrixRez;
+        private static int sdvig;
+        private static int n;
+        private static int m;
+        static void ZapolnenieMatrixRez( List<int> odinKrug, int a)
+        {
+            int i = a;
+            int j = a;  //0,0  1,1   2,2..........         
+            int pi = 0; // begunok po list
+            while (j < (n - a))  // j<kollichestva stolbcov
+            {
+                matrixRez[i, j] = odinKrug[pi];
+                j++;
+                pi++;
+            }
+            i = i + 1;
+            j = j - 1;
+            while (i < (m - a))
+            {
+                matrixRez[i, j] = odinKrug[pi];
+                i++;
+                pi++;
+            }
+            i = i - 1;
+            j = j - 1;
+            while (j >= a)
+            {
+                matrixRez[i, j] = odinKrug[pi];
+                j--;
+                pi++;
+            }
+            i = i - 1;
+            j = j + 1;
+            while (i > a)
+            {
+                matrixRez[i, j] = odinKrug[pi];
+                i--;
+                pi++;
+            }
+        }
+        static void SdvigLista(List<int> odinKrug, int a)
+        {
+            if(odinKrug.Count==0)
+                return;
+            List<int> copyOpinKrug = new List<int>();
+            int k = 0;  // index s pravel'nogo mesta s uchotom shaga
+            k = sdvig%odinKrug.Count;
+
+            if (k > 0)
+            {
+                for (int i = k; i < odinKrug.Count; i++)
+                {
+                    copyOpinKrug.Add(odinKrug[i]);
+                }
+                for (int i = 0; i < k; i++)
+                {
+                    copyOpinKrug.Add(odinKrug[i]);
+                }
+            }
+            else
+            {
+                copyOpinKrug = odinKrug;
+            }
+
+
+            ZapolnenieMatrixRez(copyOpinKrug, a);
+        }
+
+        static void Krug(int a)
+        {
+            List<int> odinKrug = new List<int>();
+            int i = a;
+            int j = a;  //0,0  1,1   2,2..........         
+            while (j < (n - a))  // j<kollichestva stolbcov
+            {
+                odinKrug.Add(matrix[i, j]);
+                j++;
+            }
+            i = i + 1;
+            j = j - 1;
+            while (i < (m - a))
+            {
+                odinKrug.Add(matrix[i, j]);
+                i++;
+            }
+            i = i - 1;
+            j = j - 1;
+            while (j >= a)
+            {
+                odinKrug.Add(matrix[i, j]);
+                j--;
+            }
+            i = i - 1;
+            j = j + 1;
+            while (i > a)
+            {
+                odinKrug.Add(matrix[i, j]);
+                i--;
+            }
+
+            SdvigLista(odinKrug, a);
+        }
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(' ');
+            m = int.Parse(str[0]);
+            n = int.Parse(str[1]);
+            sdvig = int.Parse(str[2]);
+            matrix = new int[m, n];
+            matrixRez = new int[m, n];
+            int kollichestvoKrugov;
+            if (m < n)
+            {
+                kollichestvoKrugov = m/2;
+            }
+            else
+            {
+                kollichestvoKrugov = n / 2;
+            }
+           
+                      
+            for (int i = 0; i < m; i++)
+            {
+                string[] r = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(r[j]);
+                }
+            }
+            int a = 0;
+
+            while (kollichestvoKrugov != 0)
+            {
+
+                Krug(a);
+                a++;
+                kollichestvoKrugov--;
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0} ", matrixRez[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
+
---
+++ HackerRank/Matrix Rotation/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_Rotation
+{
+    class Program
+    {
+        static int[,] matrix;
+        static int[,] matrixRez;
+        private static int sdvig;
+        private static int n;
+        private static int a;
+        private static int lengthStolbca;
+        private static int lengthStroki;
+        private static int t;
+        static void ZapolnenieMatrixRez(List<int> odinKrug, int i)
+        {
+            int counter = 0;
+            int k = t;
+
+            if (odinKrug.Count != 4)
+            {
+                while (counter < odinKrug.Count)
+                {
+                    while (k < lengthStolbca - 1)
+                    {
+                        if (i == odinKrug.Count)
+                        {
+                            i = 0;
+                        }
+                        matrixRez[a, k] = odinKrug[i];
+                        k++;
+                        i++;
+                        counter++;
+                    }
+                    k = t;
+                    while (k < lengthStroki - 1)
+                    {
+                        if (i == odinKrug.Count)
+                        {
+                            i = 0;
+                        }
+                        matrixRez[k, lengthStroki - 1] = odinKrug[i];
+                        k++;
+                        i++;
+                        counter++;
+                    }
+                    k = lengthStolbca - 1;
+                    while (k > 0)
+                    {
+                        if (i == odinKrug.Count)
+                        {
+                            i = 0;
+                        }
+                        matrixRez[lengthStolbca - 1, k] = odinKrug[i];
+                        k--;
+                        i++;
+                        counter++;
+                    }
+                    k = lengthStroki - 1;
+                    while (k > 0)
+                    {
+                        if (i == odinKrug.Count)
+                        {
+                            i = 0;
+                        }
+                        matrixRez[k, a] = odinKrug[i];
+                        k--;
+                        i++;
+                        counter++;
+                    }
+
+                }
+            }
+            else
+            {
+                while (counter < odinKrug.Count)
+                {
+
+                    if (i == odinKrug.Count)
+                    {
+                        i = 0;
+                    }
+                    matrixRez[a, k] = odinKrug[i];
+          
+                    i++;
+                    counter++;
+                    if (i == odinKrug.Count)
+                    {
+                        i = 0;
+                    }
+                    matrixRez[k, lengthStroki] = odinKrug[i];
+  
+                    i++;
+                    counter++;
+
+                    k = lengthStolbca;
+
+                    if (i == odinKrug.Count)
+                    {
+                        i = 0;
+                    }
+                    matrixRez[lengthStroki, k] = odinKrug[i];
+                    k--;
+                    i++;
+                    counter++;
+
+                    k = lengthStolbca;
+
+                    if (i == odinKrug.Count)
+                    {
+                        i = 0;
+                    }
+                    matrixRez[k, a] = odinKrug[i];
+                    k--;
+                    i++;
+                    counter++;
+
+
+                }
+            }
+
+        }
+        static void SdvigMatrix(List<int> odinKrug)
+        {
+            int counter = 0;
+            int i = 0;
+
+            while (counter != sdvig)
+            {
+                if (i == odinKrug.Count)
+                {
+                    i = 0;
+                }
+
+                counter++;
+                i++;
+            }
+
+            ZapolnenieMatrixRez(odinKrug, i);
+        }
+
+        static void Krug()
+        {
+            List<int> odinKrug = new List<int>();
+            int k = t; //uvelichivaetsya na kazhdom kruge
+            if (lengthStolbca != 2)
+            {
+                while (k < lengthStolbca - 1)
+                {
+                    odinKrug.Add(matrix[a, k]);
+                    k++;
+                }
+                k = t;
+                while (k < lengthStolbca - 1)
+                {
+                    odinKrug.Add(matrix[k, lengthStolbca - 1]);
+                    k++;
+                }
+                k = lengthStroki - 1;
+                while (k > 0)
+                {
+                    odinKrug.Add(matrix[lengthStolbca - 1, k]);
+                    k--;
+                }
+                k = lengthStroki - 1;
+                while (k > 0)
+                {
+                    odinKrug.Add(matrix[k, a]);
+                    k--;
+                }
+            }
+            else
+            {
+
+                odinKrug.Add(matrix[a, k]);
+                odinKrug.Add(matrix[k, lengthStroki]);
+
+                k = lengthStolbca;
+
+                odinKrug.Add(matrix[lengthStolbca, k]);
+                odinKrug.Add(matrix[k, a]);
+
+            }
+
+
+            SdvigMatrix(odinKrug);
+
+        }
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(' ');
+            int m = int.Parse(str[0]);
+            n = int.Parse(str[1]);
+            sdvig = int.Parse(str[2]);
+            matrix = new int[m, n];
+            matrixRez = new int[m, n];
+            int kollichestvoKrugov = n / 2;
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] r = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(r[j]);
+                }
+            }
+            a = 0;
+            lengthStolbca = n;
+            lengthStroki = m;
+            t = 0;
+            while (kollichestvoKrugov != 0)
+            {
+
+                Krug();
+                t++;
+                lengthStolbca = lengthStolbca - 2;
+                lengthStroki = lengthStroki - 2;
+                a++;
+                kollichestvoKrugov--;
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0} ",matrixRez[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
