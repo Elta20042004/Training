f7556cd7f8ac915956e48048df735719099ee91a
Elena Cherny <elta20042004@gmail.com>
2015-10-08 22:44:56 +0300

    Binary tree,rest Api

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("Elena");
+            var collection = db.GetCollection<BsonDocument>("User");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var document = new BsonDocument
+                {
+                    { "type" , "MyPups"},
+                    { "userId", i.ToString() },
+                };
+                collection.InsertOneAsync(document);
+            }
+        }
+    }
+}
---
+++ HackerRank/OMDBClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace OMDBClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httprequest = WebRequest.Create("http://www.omdbapi.com/?t=Fly");
+            var response = httprequest.GetResponse();
+            var stream = response.GetResponseStream();
+            var readStream = new StreamReader(stream);
+            var str = readStream.ReadToEnd();
+
+
+            var client = new WebClient();
+            var str2 = client.DownloadString("http://www.omdbapi.com/?t=Frozen");
+
+
+
+            dynamic deserialized = JsonConvert.DeserializeObject(str2);
+ 
+
+            string title = deserialized.Title;
+            int year = deserialized.Year;
+            string runtime = deserialized.Runtime;
+            string poster = deserialized.Poster;
+
+            str2 = client.DownloadString(poster);
+
+
+            var json = client.DownloadString("http://www.omdbapi.com/?s=House%20MD");
+           
+
+            dynamic deserializedJson = JsonConvert.DeserializeObject(json);
+
+            JArray search = deserializedJson.Search;
+            foreach (dynamic elem in search)
+            {
+                Console.WriteLine(elem.Title);
+            }
+
+
+                //string title = deserialized.Title;
+                //int year = deserialized.Year;
+                //string runtime = deserialized.Runtime;
+                //string poster = deserialized.Poster;
+
+            //str2 = client.DownloadString(poster);
+        }
+    }
+}
